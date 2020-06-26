229ae9251a8ef50582cd538320974203702b122b
Eric Simmons <eric.a.simmons573@gmail.com>
2020-06-25 19:04:45 -0700

    Added helpful links as a DM in Discord

--- WelcomeWhale.py
+++ WelcomeWhale.py
@@ -17,7 +17,15 @@ async def on_ready():
 async def on_member_join(member):
     for channel in member.guild.channels:
         if str(channel) == "general":
-            await channel.send(f"""Welcome to AI on the Beach, {member.name}! Can you tell us a little about yourself and how you found out about us?""")
+            await channel.send((f"Welcome to AI on the Beach, {member.name}!\n"
+                                "Can you tell us a little about yourself and how you found out about us? \n"))
 
+    await member.create_dm()
+    await member.dm_channel.send((
+        f"Here's some more information to help you get started:\n"
+        "Website: https://aionthebeach.com/ \n"
+        "Github: https://bit.ly/aionthebeach-code \n"
+        "Quilt: https://bit.ly/aionthebeach-data"
+        ))
 
 client.run(TOKEN)
