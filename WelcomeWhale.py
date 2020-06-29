b2664c4c6a3c6121bcf21f48cba4aff03975db60
Eric Simmons <eric.a.simmons573@gmail.com>
2020-06-29 00:35:53 -0700

    Updated direct message info for new user

--- WelcomeWhale.py
+++ WelcomeWhale.py
@@ -17,12 +17,13 @@ async def on_ready():
 async def on_member_join(member):
     for channel in member.guild.channels:
         if str(channel) == "general":
-            await channel.send((f"Welcome to AI on the Beach, {member.name}!\n"
+            await channel.send((f"Welcome to AI on the Beach, {member.name}!\n\n"
                                 "Can you tell us a little about yourself and how you found out about us? \n"))
 
     await member.create_dm()
     await member.dm_channel.send((
-        f"Here's some more information to help you get started:\n"
+        f"Hello! This is WelcomeWhale from the AI on the Beach Discord server! \n\n"
+        "Here's some more information to help you get started:\n"
         "Website: https://aionthebeach.com/ \n"
         "Github: https://bit.ly/aionthebeach-code \n"
         "Quilt: https://bit.ly/aionthebeach-data"
